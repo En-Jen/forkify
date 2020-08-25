8bed241f44ce49506a14193db6da0ff0c1d8e928
Jen Speak <jspeak6@gmail.com>
2020-08-25 13:57:42 -0700

    Converted ingredient counts to fractions & added active class for selected search results

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -13,6 +13,17 @@ export const clearResults = () => {
     elements.searchResPages.innerHTML = '';
 };
 
+export const highlightedSelected = id => {
+    // Remove active class from all search results
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    })
+
+    // Add active class to selected search result
+    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+};
+
 /*
 // 'Pasta with tomato and spinach'
 acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
