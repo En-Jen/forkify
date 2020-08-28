5391c836e2519a4f412830d61eaa973b9bc24a7c
Jen Speak <jspeak6@gmail.com>
2020-08-27 22:41:17 -0700

    Implemented persistent data w/ localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -149,10 +149,6 @@ elements.shopping.addEventListener('click', e => {
 /**
  * LIKE CONTROLLER
  */
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     // Create a new likes object IF there is none yet
     if (!state.likes) state.likes = new Likes();
@@ -189,6 +185,20 @@ const controlLike = () => {
 
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 // Handling recipe button clicks
